fb6c4d15feeed6db82931eda3f14317b5b244953
vjchi <31632164+Vjchi@users.noreply.github.com>
2021-01-16 11:55:15 +0000

    lsight changes

--- client/src/App.js
+++ client/src/App.js
@@ -17,7 +17,7 @@ function App() {
             target="_blank"
             rel="noopener noreferrer"
           >
-            Learn React
+            Learn React, and try some new stuff!
           </a>
           <Link to="/">Home</Link>
           <Link to="/otherpage">Other Page</Link>
